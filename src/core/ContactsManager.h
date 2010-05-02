7f3fcf9184fbde9ee0fbaeb70848725b81c6b76b
John Brooks <special@dereferenced.net>
2010-05-01 20:28:50 -0600

    Added the GPLv2 license text and notices

--- src/core/ContactsManager.h
+++ src/core/ContactsManager.h
@@ -1,3 +1,20 @@
+/* TorIM - http://gitorious.org/torim
+ * Copyright (C) 2010, John Brooks <special@dereferenced.net>
+ *
+ * TorIM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TorIM. If not, see http://www.gnu.org/licenses/
+ */
+
 #ifndef CONTACTSMANAGER_H
 #define CONTACTSMANAGER_H
 
