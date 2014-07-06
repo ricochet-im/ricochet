ricochet-im/ricochet

commit 3f3b857dee0c95c428bc8f9479a34d3fd85fc0ab

core: Change address format to ricochet:
Addresses beginning with 'torsion:' will still be accepted, but not
generated.

src/core/ContactIDValidator.cpp
@@ -32,7 +32,7 @@
 
 #include "ContactIDValidator.h"
 
-static QRegularExpression regex(QStringLiteral("torsion:[a-z2-7]{16}"));
+static QRegularExpression regex(QStringLiteral("(torsion|ricochet):[a-z2-7]{16}"));
 
 ContactIDValidator::ContactIDValidator(QObject *parent)
     : QRegularExpressionValidator(parent), m_uniqueIdentity(0)
@@ -105,7 +105,7 @@ QString ContactIDValidator::idFromHostname(const QString &hostname)
             return QString();
     }
 
-    re.prepend(QStringLiteral("torsion:"));
+    re.prepend(QStringLiteral("ricochet:"));
 
     if (!isValidID(re))
         return QString();

src/core/ContactUser.h
@@ -97,7 +97,7 @@ public:
     /* Hostname is in the onion hostname format, i.e. it ends with .onion */
     QString hostname() const;
     quint16 port() const;
-    /* Contact ID in the torsion: format */
+    /* Contact ID in the ricochet: format */
     QString contactID() const;
 
     Status status() const { return m_status; }

src/core/ContactsManager.h
@@ -66,7 +66,7 @@ public:
     ContactUser *lookupUniqueID(int uniqueID) const;
 
     /* Create a new user and a contact request for that user. Use this instead of addContact.
-     * Note that contactID should be an torsion: ID. */
+     * Note that contactID should be an ricochet: ID. */
     Q_INVOKABLE ContactUser *createContactRequest(const QString &contactID, const QString &nickname,
                                                   const QString &myNickname, const QString &message);
 

src/ui/LinkedText.cpp
@@ -45,7 +45,8 @@ LinkedText::LinkedText(QObject *parent)
 
     allowedSchemes << QStringLiteral("http")
                    << QStringLiteral("https")
-                   << QStringLiteral("torsion");
+                   << QStringLiteral("torsion")
+                   << QStringLiteral("ricochet");
 }
 
 QString LinkedText::parsed(const QString &input)

src/ui/qml/ContactIDField.qml
@@ -25,7 +25,7 @@ FocusScope {
             Layout.fillWidth: true
             font.family: "Courier"
             validator: readOnly ? null : idValidator
-            placeholderText: "torsion:"
+            placeholderText: "ricochet:"
             focus: true
 
             onTextChanged: errorBubble.clear()
@@ -41,7 +41,7 @@ FocusScope {
                     else if (matchesIdentity(field.text))
                         errorBubble.show(qsTr("You can't add yourself as a contact"))
                     else
-                        errorBubble.show(qsTr("Enter an ID starting with <b>torsion:</b>"))
+                        errorBubble.show(qsTr("Enter an ID starting with <b>ricochet:</b>"))
                 }
             }
 

src/ui/qml/ContactRequestDialog.qml
@@ -80,7 +80,7 @@ ApplicationWindow {
         readOnly: true
 
         Component.onCompleted: {
-            contactId.text = "torsion:" + request.hostname
+            contactId.text = "ricochet:" + request.hostname
             name.text = request.nickname
             name.readOnly = false
             name.focus = true

src/ui/qml/MessageDelegate.qml
@@ -126,7 +126,8 @@ Column {
             MenuItem {
                 id: linkAddContact
                 text: qsTr("Add as Contact")
-                visible: hoveredLink.length > 0 && hoveredLink.substr(0,8).toLowerCase() == "torsion:"
+                visible: hoveredLink.length > 0 && (hoveredLink.substr(0,9).toLowerCase() == "ricochet:"
+                                                    || hoveredLink.substr(0,8).toLowerCase() == "torsion:")
                 onTriggered: {
                     var object = createDialog("AddContactDialog.qml", { 'staticContactId': hoveredLink }, chatWindow)
                     object.visible = true

commit 131d8e564e452dfcce00bb07e1f67cc8b8e3eb67

core: Fix contact ID parsing with new URI name
Regression from 3f3b857, which prevented adding new contacts.

src/core/ContactIDValidator.cpp
@@ -32,7 +32,7 @@
 
 #include "ContactIDValidator.h"
 
-static QRegularExpression regex(QStringLiteral("(torsion|ricochet):[a-z2-7]{16}"));
+static QRegularExpression regex(QStringLiteral("(torsion|ricochet):([a-z2-7]{16})"));
 
 ContactIDValidator::ContactIDValidator(QObject *parent)
     : QRegularExpressionValidator(parent), m_uniqueIdentity(0)
@@ -72,7 +72,7 @@ ContactUser *ContactIDValidator::matchingContact(const QString &text) const
 
 bool ContactIDValidator::matchesIdentity(const QString &text) const
 {
-    return m_uniqueIdentity && m_uniqueIdentity->contactID() == text;
+    return m_uniqueIdentity && m_uniqueIdentity->hostname() == hostnameFromID(text);
 }
 
 void ContactIDValidator::fixup(QString &text) const
@@ -87,10 +87,11 @@ bool ContactIDValidator::isValidID(const QString &text)
 
 QString ContactIDValidator::hostnameFromID(const QString &ID)
 {
-    if (!isValidID(ID))
+    QRegularExpressionMatch match = regex.match(ID);
+    if (!match.hasMatch())
         return QString();
 
-    return ID.mid(8) + QStringLiteral(".onion");
+    return match.captured(2) + QStringLiteral(".onion");
 }
 
 QString ContactIDValidator::idFromHostname(const QString &hostname)