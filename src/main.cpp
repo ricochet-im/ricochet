ricochet-im/ricochet

commit a9829c60f1b4e9e0ea39a08a1a6d78d0a2978dcc

utils: Move config locking out of Settings

src/main.cpp
@@ -45,10 +45,11 @@
 #include <QTranslator>
 #include <QMessageBox>
 #include <QLocale>
+#include <QLockFile>
 #include <QStandardPaths>
 #include <openssl/crypto.h>
 
-static bool initSettings(SettingsFile *settings, QString &errorMessage);
+static bool initSettings(SettingsFile *settings, QLockFile **lockFile, QString &errorMessage);
 static bool importLegacySettings(SettingsFile *settings, const QString &oldPath);
 static void initTranslation();
 
@@ -62,13 +63,13 @@ int main(int argc, char *argv[])
     QScopedPointer<SettingsFile> settings(new SettingsFile);
     SettingsObject::setDefaultFile(settings.data());
 
-    {
-        QString error;
-        if (!initSettings(settings.data(), error)) {
-            QMessageBox::critical(0, qApp->translate("Main", "Ricochet Error"), error);
-            return 1;
-        }
+    QString error;
+    QLockFile *lock = 0;
+    if (!initSettings(settings.data(), &lock, error)) {
+        QMessageBox::critical(0, qApp->translate("Main", "Ricochet Error"), error);
+        return 1;
     }
+    QScopedPointer<QLockFile> lockFile(lock);
 
     /* Initialize OpenSSL's allocator */
     CRYPTO_malloc_init();
@@ -117,7 +118,7 @@ static QString appBundlePath()
 }
 #endif
 
-static bool initSettings(SettingsFile *settings, QString &errorMessage)
+static bool initSettings(SettingsFile *settings, QLockFile **lockFile, QString &errorMessage)
 {
     /* If built in portable mode (default), configuration is stored in the 'config'
      * directory next to the binary. If not writable, launching fails.
@@ -153,6 +154,20 @@ static bool initSettings(SettingsFile *settings, QString &errorMessage)
     }
 
     QDir dir(configPath);
+
+    QLockFile *lock = new QLockFile(dir.filePath(QStringLiteral("ricochet.json.lock")));
+    *lockFile = lock;
+    lock->setStaleLockTime(0);
+    if (!lock->tryLock()) {
+        if (lock->error() == QLockFile::LockFailedError) {
+            // XXX Need to offer UI or heuristics for removeStaleLockFile
+            errorMessage = QStringLiteral("Configuration file is already in use");
+        } else {
+            errorMessage = QStringLiteral("Cannot write configuration file (failed to acquire lock)");
+        }
+        return false;
+    }
+
     settings->setFilePath(dir.filePath(QStringLiteral("ricochet.json")));
     if (settings->hasError()) {
         errorMessage = settings->errorMessage();

src/utils/Settings.cpp
@@ -34,7 +34,6 @@
 #include <QCoreApplication>
 #include <QJsonDocument>
 #include <QJsonParseError>
-#include <QLockFile>
 #include <QSaveFile>
 #include <QFile>
 #include <QDir>
@@ -51,7 +50,6 @@ public:
     SettingsFile *q;
     QString filePath;
     QString errorMessage;
-    QLockFile *lock;
     QTimer syncTimer;
     QJsonObject jsonRoot;
     SettingsObject *rootObject;
@@ -62,7 +60,6 @@ public:
     void reset();
     void setError(const QString &message);
     bool checkDirPermissions(const QString &path);
-    bool acquireLock(const QString &path);
     bool readFile();
     bool writeFile();
 
@@ -90,7 +87,6 @@ SettingsFile::~SettingsFile()
 SettingsFilePrivate::SettingsFilePrivate(SettingsFile *qp)
     : QObject(qp)
     , q(qp)
-    , lock(0)
     , rootObject(0)
 {
     syncTimer.setInterval(0);
@@ -102,7 +98,6 @@ SettingsFilePrivate::~SettingsFilePrivate()
 {
     if (syncTimer.isActive())
         sync();
-    delete lock;
     delete rootObject;
 }
 
@@ -110,8 +105,6 @@ void SettingsFilePrivate::reset()
 {
     filePath.clear();
     errorMessage.clear();
-    delete lock;
-    lock = 0;
 
     jsonRoot = QJsonObject();
     emit modified(QStringList(), jsonRoot);
@@ -137,9 +130,6 @@ bool SettingsFile::setFilePath(const QString &filePath)
         return false;
     }
     d->checkDirPermissions(fileInfo.path());
-
-    if (!d->acquireLock(filePath + QStringLiteral(".lock")))
-        return false;
 
     if (!d->readFile())
         return false;
@@ -175,31 +165,6 @@ bool SettingsFilePrivate::checkDirPermissions(const QString &path)
             qWarning() << "Correcting permissions on configuration directory failed";
             return false;
         }
-    }
-
-    return true;
-}
-
-bool SettingsFilePrivate::acquireLock(const QString &path)
-{
-    if (lock) {
-        setError(QStringLiteral("Lock is already held"));
-        return false;
-    }
-
-    lock = new QLockFile(path);
-    lock->setStaleLockTime(0);
-    if (!lock->tryLock()) {
-        if (lock->error() == QLockFile::LockFailedError) {
-            // XXX Should offer a way to removeStaleLockFile() from the UI
-            setError(QStringLiteral("Configuration file is already in use"));
-        } else {
-            setError(QStringLiteral("Cannot write configuration file (failed to acquire lock)"));
-        }
-
-        delete lock;
-        lock = 0;
-        return false;
     }
 
     return true;

src/utils/Settings.h
@@ -46,10 +46,8 @@ class SettingsObjectPrivate;
 
 /* SettingsFile represents a JSON-encoded configuration file.
  *
- * SettingsFile handles atomic write and file locking over a settings
- * file, which is a JSON file with a root object. The lock is held
- * while the SettingsFile instance exists, and will prevent other
- * processes from using that configuration simultaneously.
+ * SettingsFile is an API for reading, writing, and change notification
+ * on JSON-encoded settings files.
  *
  * Data is accessed via SettingsObject, either using the root property
  * or by creating a SettingsObject, optionally using a base path.

commit 7e0ab229192f138d63e0226db0172cf5995d3ead

core: Ensure config directory exists before getting lock
Fixes startup on first time use, regression from a9829c60

src/main.cpp
@@ -154,6 +154,10 @@ static bool initSettings(SettingsFile *settings, QLockFile **lockFile, QString &
     }
 
     QDir dir(configPath);
+    if (!dir.exists() && !dir.mkpath(QStringLiteral("."))) {
+        errorMessage = QStringLiteral("Cannot create directory: %1").arg(dir.path());
+        return false;
+    }
 
     QLockFile *lock = new QLockFile(dir.filePath(QStringLiteral("ricochet.json.lock")));
     *lockFile = lock;