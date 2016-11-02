6583a3c8bdd4598c2c554c2fe272385bb027ee18
John Brooks <john.brooks@dereferenced.net>
2016-11-02 16:05:06 -0600

    Use self-contained paths in installer
    This makes it possible to build the installer from other locations.

--- packaging/installer/prepare.bat
+++ packaging/installer/prepare.bat
@@ -1,3 +1,8 @@
 if exist "Qt\" rd /q /s Qt
 windeployqt --qmldir ..\..\src\ui\qml --dir Qt ricochet.exe
 if exist "Qt\qmltooling" rd /q /s Qt\qmltooling
+copy /Y ..\..\icons\ricochet.ico ricochet.ico
+copy /Y ..\..\LICENSE LICENSE
+if not exist "translation\" mkdir translation
+copy /Y ..\..\translation\installer_*.isl translation\
+xcopy /Y ..\..\translation\inno translation\inno\
