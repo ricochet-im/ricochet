79c79a0398de1557c7c572327247eaa31e3a7942
Bernard Spil <brnrd@FreeBSD.org>
2017-02-25 12:58:07 +0100

    Fix build with LibreSSL
     - Check LIBRESSL_VERSION_NUMBER in addition to OpenSSL > 1.1
    
    LibreSSL forked from OpenSSL 1.0.1f and does not have all features
    from 1.1.0.

--- src/utils/CryptoKey.cpp
+++ src/utils/CryptoKey.cpp
@@ -39,7 +39,7 @@
 #include <openssl/bio.h>
 #include <openssl/pem.h>
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 void RSA_get0_factors(const RSA *r, const BIGNUM **p, const BIGNUM **q)
 {
   *p = r->p;
