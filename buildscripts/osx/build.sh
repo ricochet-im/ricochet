893bac89c516d71b56aed73c791fd832c564a4e3
Derwent <derwentx@gmail.com>
2019-07-20 16:56:35 +1000

    make macos build use local openssl

--- buildscripts/osx/build.sh
+++ buildscripts/osx/build.sh
@@ -5,6 +5,8 @@ set -ex
 ROOT_LIB=$(pwd)/lib
 BUILD_OUTPUT=$(pwd)/output
 
+OPENSSLDIR="${OPENSSLDIR:-${ROOT_LIB}/openssl/}"
+
 test -e "${BUILD_OUTPUT}/Ricochet Refresh.app" && rm -rf "${BUILD_OUTPUT}/Ricochet Refresh.app"
 test -e "${BUILD_OUTPUT}/ricochet-refresh-unstripped" && rm -rf "${BUILD_OUTPUT}/ricochet-refresh-unstripped"
 test -e "${BUILD_OUTPUT}/Ricochet*.dmg" && rm -rf "${BUILD_OUTPUT}/Ricochet*.dmg"
@@ -19,7 +21,7 @@ pushd ..
 
     export PKG_CONFIG_PATH=${ROOT_LIB}/protobuf/lib/pkgconfig:${PKG_CONFIG_PATH}
     export PATH=${ROOT_LIB}/protobuf/bin/:${PATH}
-    qmake CONFIG+=release OPENSSLDIR="${ROOT_LIB}/openssl/" ..
+    qmake CONFIG+=release OPENSSLDIR="$OPENSSLDIR" ..
     make ${MAKEOPTS}
 
     cp ricochet-refresh.app/Contents/MacOS/ricochet-refresh "${BUILD_OUTPUT}/ricochet-refresh-unstripped"
