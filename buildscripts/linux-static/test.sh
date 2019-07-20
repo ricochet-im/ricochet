1a63680238e82cdceb49fdc15264e608f71da142
Derwent <derwentx@gmail.com>
2019-07-20 20:17:26 +1000

    use xvfb to test qt on linux

--- buildscripts/linux-static/test.sh
+++ buildscripts/linux-static/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 ROOT_LIB=$(pwd)/lib
 OPENSSLDIR="${OPENSSLDIR:-${ROOT_LIB}/openssl/}"
@@ -9,17 +9,13 @@ pushd "../tests"
   export PKG_CONFIG_PATH=${ROOT_LIB}/protobuf/lib/pkgconfig:${PKG_CONFIG_PATH}
   export PATH=${ROOT_LIB}/protobuf/bin/:${PATH}
 
-  MAKE_COMMAND=$'qmake tests.pro CONFIG+=x86_64 CONFIG+=qtquickcompiler OPENSSLDIR=$OPENSSLDIR && make qmake_all && make'
+  qmake tests.pro CONFIG+=x86_64 CONFIG+=qtquickcompiler OPENSSLDIR=$OPENSSLDIR && make qmake_all && make
   TEST_COMMAND=$'find . -type f -regextype sed -regex "./.*\(test_\|tst_\)[^/]*" -executable | while read -r test; do $test || exit $?; done'
 
   if [ -n "$HEADLESS" ]; then
-    docker pull garthk/qt-build:bionic-5.12.0
-    docker run -d -v $PWD/..:/repo -w /repo/tests --name tester garthk/qt-build:bionic-5.12.0 tail -f /dev/null
-    docker exec -it tester bash -c "apt-get update && apt-get -yq --no-install-suggests --no-install-recommends install libssl-dev libprotobuf-dev protobuf-compiler qt5-qmake qt5-default qtbase5-dev qttools5-dev-tools qtdeclarative5-dev pkg-config libxcb-xinerama0"
-    docker exec -it --env "PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig" tester bash -c "$MAKE_COMMAND"
-    docker exec -it tester bash -c "$TEST_COMMAND"
+    Xvfb :1 -screen 0 800x600x16 &
+    xvfb-run "$TEST_COMMAND"
   else
-    "$MAKE_COMMAND"
     "$TEST_COMMAND"
   fi
 popd
