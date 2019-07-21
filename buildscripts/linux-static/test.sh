548e06826717d18a2e81f678539a42a8a89ecdcc
Derwent <derwentx@gmail.com>
2019-07-21 10:24:59 +1000

    tidier linux tests

--- buildscripts/linux-static/test.sh
+++ buildscripts/linux-static/test.sh
@@ -9,13 +9,14 @@ pushd "../tests"
   export PKG_CONFIG_PATH=${ROOT_LIB}/protobuf/lib/pkgconfig:${PKG_CONFIG_PATH}
   export PATH=${ROOT_LIB}/protobuf/bin/:${PATH}
 
-  qmake tests.pro CONFIG+=x86_64 CONFIG+=qtquickcompiler OPENSSLDIR=$OPENSSLDIR && make qmake_all && make
+  qmake tests.pro CONFIG+=x86_64 CONFIG+=qtquickcompiler "OPENSSLDIR=$OPENSSLDIR" && make qmake_all && make
+  TESTS=$(find . -type f -regextype sed -regex "./.*\(test_\|tst_\)[^/]*" -executable)
 
   if [ -n "$HEADLESS" ]; then
     Xvfb :1 -screen 0 800x600x16 &
-    find . -type f -regextype sed -regex "./.*\(test_\|tst_\)[^/]*" -executable | while read -r test; do xvfb-run $test || exit $?; done
+    echo "$TESTS" | while read -r test; do xvfb-run "$test" || exit $?; done
   else
-    find . -type f -regextype sed -regex "./.*\(test_\|tst_\)[^/]*" -executable | while read -r test; do $test || exit $?; done
+    echo "$TESTS" | while read -r test; do "$test" || exit $?; done
   fi
 popd
 
