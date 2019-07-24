ac583f111d98dbcff8665248911875399d0f841e
Derwent <derwentx@gmail.com>
2019-07-24 13:56:12 +1000

    include qt in path for test

--- buildscripts/linux-static/test.sh
+++ buildscripts/linux-static/test.sh
@@ -7,7 +7,7 @@ OPENSSLDIR="${OPENSSLDIR:-${ROOT_LIB}/openssl/}"
 
 pushd "../tests"
   export PKG_CONFIG_PATH=${ROOT_LIB}/protobuf/lib/pkgconfig:${PKG_CONFIG_PATH}
-  export PATH=${ROOT_LIB}/protobuf/bin/:${PATH}
+  export PATH="${ROOT_LIB}/protobuf/bin/:${ROOT_LIB}/qt5/bin:${PATH}"
 
   qmake tests.pro CONFIG+=x86_64 CONFIG+=qtquickcompiler "OPENSSLDIR=$OPENSSLDIR" && make qmake_all && make
   TESTS=$(find . -type f -regextype sed -regex "./.*\(test_\|tst_\)[^/]*" -executable)
