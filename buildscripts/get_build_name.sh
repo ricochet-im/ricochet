8ad9468b414a949b39206f2ed1d7fa91fa1be7d5
Derwent <derwentx@gmail.com>
2019-07-26 15:48:03 +1000

    more detailed build name

--- buildscripts/get_build_name.sh
+++ buildscripts/get_build_name.sh
@@ -2,4 +2,7 @@
 
 source common_functions.sh
 
-git_version
+RICOCHET_VERSION="$(git_version)"
+TOR_VERSION="$(cd src/tor && git_version)"
+OPENSSL_VERSION="$(cd src/openssl && git_version)"
+echo "ricochet-${RICOCHET_VERSION} tor-${TOR_VERSION} openssl-${OPENSSL_VERSION}"
