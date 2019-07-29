81fe44eff4e063dd8b518f207c41fba239830236
Derwent <derwentx@gmail.com>
2019-07-29 21:36:53 +1000

    linux-static: add build arguments, debug, fontdir

--- buildscripts/linux-static/build.sh
+++ buildscripts/linux-static/build.sh
@@ -5,6 +5,7 @@ set -ex
 source common_functions.sh
 ROOT_LIB=$(pwd)/lib
 BUILD_OUTPUT=$(pwd)/output
+MAKEOPTS="-j$(nproc)"
 
 pushd ..
   RICOCHET_VERSION=$(git_version)
@@ -13,9 +14,11 @@ pushd ..
   mkdir build
   pushd build
 
-    export PKG_CONFIG_PATH=${ROOT_LIB}/protobuf/lib/pkgconfig:${PKG_CONFIG_PATH}
-    export PATH=${ROOT_LIB}/qt5/bin/:${ROOT_LIB}/protobuf/bin/:${PATH}
-    qmake CONFIG+=release OPENSSLDIR="${ROOT_LIB}/openssl/" ..
+    export PKG_CONFIG_PATH="${ROOT_LIB}/protobuf/lib/pkgconfig:${PKG_CONFIG_PATH}"
+    export PATH="${ROOT_LIB}/qt5/bin/:${ROOT_LIB}/protobuf/bin/:${PATH}"
+    export QT_QPA_FONTDIR=/usr/share/fonts/truetype/dejavu
+    printf "qmake -query: \n%s" "$(qmake -query)"
+    qmake CONFIG+=debug OPENSSLDIR="${ROOT_LIB}/openssl/" ..
     make ${MAKEOPTS}
     cp ricochet-refresh "${BUILD_OUTPUT}/ricochet-refresh-unstripped"
     strip ricochet-refresh
@@ -23,7 +26,10 @@ pushd ..
     mkdir -p staging/ricochet-refresh
     cp ricochet-refresh staging/ricochet-refresh
     cp "${BUILD_OUTPUT}/tor" staging/ricochet-refresh
-    cp -r ../packaging/linux-static/content/* staging/ricochet-refresh/
+    # cp -r ../packaging/linux-static/content/* staging/ricochet-refresh/
+
+    echo "ldd"
+    ldd ricochet-refresh
 
     pushd staging
       tar cfj "${BUILD_OUTPUT}/ricochet-refresh-${RICOCHET_VERSION}-static.tar.bz2" ricochet-refresh
