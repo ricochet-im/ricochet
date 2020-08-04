7f177faf261eb5694ea41fe5d07fbb351a3a23ca
m-simonelli <60584156+m-simonelli@users.noreply.github.com>
2020-08-04 21:26:57 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,7 @@ int main(int argc, char *argv[])
     if (qEnvironmentVariableIsEmpty("QMLSCENE_DEVICE"))
         qputenv("QMLSCENE_DEVICE", "softwarecontext");
 
+    /* https://doc.qt.io/qt-5/highdpi.html#high-dpi-support-in-qt */
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     if (!qEnvironmentVariableIsSet("QT_DEVICE_PIXEL_RATIO")
             && !qEnvironmentVariableIsSet("QT_AUTO_SCREEN_SCALE_FACTOR")
