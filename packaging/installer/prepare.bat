7979cc79572779804e9db68aa6c4647fc0707f6e
Marco Simonelli <msimonelli@protonmail.com>
2021-11-16 22:35:17 +1100

    Removed packaging directory, in favour of ricochet-build

--- packaging/installer/prepare.bat
+++
@@ -1,8 +0,0 @@
-if exist "Qt\" rd /q /s Qt
-windeployqt --qmldir ..\..\src\ui\qml --dir Qt ricochet.exe
-if exist "Qt\qmltooling" rd /q /s Qt\qmltooling
-copy /Y ..\..\icons\ricochet_refresh.ico ricochet_refresh.ico
-copy /Y ..\..\LICENSE LICENSE
-if not exist "translation\" mkdir translation
-copy /Y ..\..\translation\installer_*.isl translation\
-xcopy /Y ..\..\translation\inno translation\inno\
