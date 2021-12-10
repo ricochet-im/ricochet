5766a48a1769d59b5fdc75fab50626917a5471fe
Marco Simonelli <msimonelli@protonmail.com>
2021-12-10 17:50:55 +1100

    Build: update prefix path for x86_64-apple-darwin

--- src/cmake/toolchains/x86_64-apple-darwin.cmake
+++ src/cmake/toolchains/x86_64-apple-darwin.cmake
@@ -1,4 +1,8 @@
 set(CMAKE_SYSTEM_NAME Darwin CACHE STRING "" FORCE)
 set(CMAKE_SYSTEM_PROCESSOR x86_64 CACHE STRING "" FORCE)
 
-set(CMAKE_PREFIX_PATH "/var/tmp/dist/macosx-toolchain/clang/lib/;/var/tmp/dist/macosx-toolchain/MacOSX10.15.sdk/" CACHE STRING "" FORCE)
+set(CMAKE_C_COMPILER_TARGET "x86_64-apple-darwin")
+set(CMAKE_CXX_COMPILER_TARGET "x86_64-apple-darwin")
+set(CMAKE_ASM_COMPILER_TARGET "x86_64-apple-darwin")
+
+set(CMAKE_PREFIX_PATH "/var/tmp/dist/macosx-toolchain/clang/lib/;/var/tmp/dist/macosx-toolchain/MacOSX10.15.sdk/System/Library/Frameworks" CACHE STRING "" FORCE)
