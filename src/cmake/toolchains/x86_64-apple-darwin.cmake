f9df6613f74742833b7325e44d700873a8c72f19
Marco Simonelli <msimonelli@protonmail.com>
2021-12-10 17:25:31 +1100

    Build: update x86_64-apple-darwin toolchain file

--- src/cmake/toolchains/x86_64-apple-darwin.cmake
+++ src/cmake/toolchains/x86_64-apple-darwin.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_SYSTEM_NAME Darwin CACHE STRING "" FORCE)
 set(CMAKE_SYSTEM_PROCESSOR x86_64 CACHE STRING "" FORCE)
 
-set(CMAKE_PREFIX_PATH "/var/tmp/dist/macosx-toolchain/clang/lib/" CACHE STRING "" FORCE)
+set(CMAKE_PREFIX_PATH "/var/tmp/dist/macosx-toolchain/clang/lib/;/var/tmp/dist/macosx-toolchain/MacOSX10.15.sdk/" CACHE STRING "" FORCE)
