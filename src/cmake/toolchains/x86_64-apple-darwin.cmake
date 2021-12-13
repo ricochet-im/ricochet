e1cc61f7cca6dff1977bf781b7bd461fd0553d25
Marco Simonelli <msimonelli@protonmail.com>
2021-12-13 14:33:07 +1100

    Build: Update x86_64-apple-darwin toolchain file

--- src/cmake/toolchains/x86_64-apple-darwin.cmake
+++ src/cmake/toolchains/x86_64-apple-darwin.cmake
@@ -5,4 +5,5 @@ set(CMAKE_C_COMPILER_TARGET "x86_64-apple-darwin")
 set(CMAKE_CXX_COMPILER_TARGET "x86_64-apple-darwin")
 set(CMAKE_ASM_COMPILER_TARGET "x86_64-apple-darwin")
 
-set(CMAKE_PREFIX_PATH "/var/tmp/dist/macosx-toolchain/clang/lib/;/var/tmp/dist/macosx-toolchain/MacOSX10.15.sdk/System/Library/Frameworks" CACHE STRING "" FORCE)
+set(CMAKE_PREFIX_PATH "/var/tmp/dist/macosx-toolchain/clang/lib/" CACHE STRING "" FORCE)
+set(CMAKE_FRAMEWORK_PATH "/var/tmp/dist/macosx-toolchain/MacOSX10.15.sdk/System/Library/Frameworks" CACHE STRING "" FORCE)
